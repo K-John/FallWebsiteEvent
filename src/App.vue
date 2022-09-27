0b95beeb48bb97bbe4f0900b0ed6cea8f1b35db9
Kendall Johnson <kenjohnson1994@gmail.com>
2022-09-26 22:04:40 -0500

    Add FallEvent and characters to default app component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,40 @@
 <template>
+  <FallEvent :characters="characters"></FallEvent>
 </template>
 
+<script setup>
+import FallEvent from './components/FallEvent.vue';
+
+let characters = [
+  {
+    name: 'Kendall J.',
+    username: 'kendall.johnson',
+    image: 'https://techlink.cmcss.net/Login/GetPhoto?PersonId=6323'
+  },
+  {
+    name: 'Emily D.',
+    username: 'emily.demorest',
+    image: 'https://techlink.cmcss.net/Login/GetPhoto?PersonId=5739'
+  },
+  {
+    name: 'Tiffany O.',
+    username: 'tiffany.owen',
+    image: 'https://techlink.cmcss.net/Login/GetPhoto?PersonId=3'
+  },
+  {
+    name: 'Eric S.',
+    username: 'eric.salyer',
+    image: 'https://techlink.cmcss.net/Login/GetPhoto?PersonId=5'
+  },
+  {
+    name: 'Joe W.',
+    username: 'joe.williams',
+    image: 'https://techlink.cmcss.net/Login/GetPhoto?PersonId=2'
+  },
+  {
+    name: 'Pumpkin',
+    username: 'pumpkin',
+    image: '/src/assets/pumpkin.svg'
+  }
+];
+</script>
