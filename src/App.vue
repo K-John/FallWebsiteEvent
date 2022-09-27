3c6ae68b3c8319d86c40f58c908a56991b3adf02
Kendall Johnson <kenjohnson1994@gmail.com>
2022-09-27 07:54:24 -0500

    Modify character list

--- src/App.vue
+++ src/App.vue
@@ -7,29 +7,14 @@ import FallEvent from './components/FallEvent.vue';
 
 let characters = [
   {
-    name: 'Kendall J.',
-    username: 'kendall.johnson',
-    image: 'https://techlink.cmcss.net/Login/GetPhoto?PersonId=6323'
+    name: 'Pumpkin',
+    username: 'pumpkin',
+    image: '/src/assets/pumpkin.svg'
   },
   {
-    name: 'Emily D.',
-    username: 'emily.demorest',
-    image: 'https://techlink.cmcss.net/Login/GetPhoto?PersonId=5739'
-  },
-  {
-    name: 'Tiffany O.',
-    username: 'tiffany.owen',
-    image: 'https://techlink.cmcss.net/Login/GetPhoto?PersonId=3'
-  },
-  {
-    name: 'Eric S.',
-    username: 'eric.salyer',
-    image: 'https://techlink.cmcss.net/Login/GetPhoto?PersonId=5'
-  },
-  {
-    name: 'Joe W.',
-    username: 'joe.williams',
-    image: 'https://techlink.cmcss.net/Login/GetPhoto?PersonId=2'
+    name: 'Pumpkin',
+    username: 'pumpkin',
+    image: '/src/assets/pumpkin.svg'
   },
   {
     name: 'Pumpkin',
